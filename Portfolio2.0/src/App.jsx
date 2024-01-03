d76cf26ce596d8f866b5dc9985affdf2f5f7797e
CambiG1123 <campbellgilliland@gmail.com>
2024-01-03 17:26:29 -0500

    created the Homepage layout, navbar, about me section

---
+++ Portfolio2.0/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import About from './components/About/About'
+
+function App() {
+  
+  return (
+    <>
+   <NavBar />
+   <Home />
+   <About />
+    </>
+  )
+}
+
+export default App
---
+++ Portfolio2.0/src/components/NavBar.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+import { FiAlignCenter, FiMail } from "react-icons/fi";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { FiBriefcase } from "react-icons/fi";
+import { FiUser } from "react-icons/fi";
+
+const NavBar = () => {
+  const [showNav, setShowNav] = useState(false);
+  const handleNav = () => {
+    setShowNav(!showNav);
+  };
+  return (
+    <div>
+      <FiAlignCenter
+        onClick={handleNav}
+        className="absolute top-4 right-4 z-[99] md:hidden "
+      />
+      {showNav ? (
+        <div className="fixed w-full h-screen bg-gray-500/90 flex flex-col justify-center items-center z-20">
+          <a
+            href="#home"
+            className="w-{75%} flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineHome size={25} />
+            <span className="pl-4">Home</span>
+          </a>
+          <a
+            href="#about"
+            className="w-{75%} flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <FiUser size={25} />
+            <span className="pl-4">About</span>
+          </a>
+          <a
+            href="#projects"
+            className="w-{75%} flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <FiBriefcase size={25} />
+            <span className="pl-4">Projects</span>
+          </a>
+          <a
+            href="#contact"
+            className="w-{75%} flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineMail size={25} />
+            <span className="pl-4">Contact</span>
+          </a>
+        </div>
+      ) : (
+        <div className="md:block hidden fixed top-[25%] z-10 ">
+          <div className="flex flex-col">
+            <a
+              href="#main"
+              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            >
+              <AiOutlineHome size={25} />
+            </a>
+            <a
+              href="#about"
+              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            >
+              <FiUser size={25} />
+            </a>
+            <a
+              href="#projects"
+              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            >
+              <FiBriefcase size={25} />
+            </a>
+            <a
+              href="#contact"
+              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            >
+              <AiOutlineMail size={25} />
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
